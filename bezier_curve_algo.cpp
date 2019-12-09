d86585d534aee34cd2f02e275aa9fe45dfec3164
Sridevi Alawandi <sridevialawandi@gmail.com>
2019-12-09 17:27:53 +0000

    ic

--- bezier_curve_algo.cpp
+++ bezier_curve_algo.cpp
@@ -14,6 +14,20 @@ typedef struct wcPt3D
 	GLfloat x, y, z;
 };
 
+void computeBezPt(GLfloat u, wcPt3D* bezPt, GLint nCtrlPts, wcPt3D* ctrlPts, GLint* C)
+{
+	GLint k, n = nCtrlPts - 1;
+	GLfloat bezBlendFcn;
+	bezPt->x = bezPt->y = bezPt->z = 0.0;
+	for (k = 0; k < nCtrlPts; k++)
+	{
+		bezBlendFcn = C[k] * pow(u, k) * pow(1 - u, n - k);
+		bezPt->x += ctrlPts[k].x * bezBlendFcn;
+		bezPt->y += ctrlPts[k].y * bezBlendFcn;
+		bezPt->z += ctrlPts[k].z * bezBlendFcn;
+	}
+}
+
 void bino(GLint n, GLint* C)
 {
 	GLint k, j;
@@ -27,17 +41,16 @@ void bino(GLint n, GLint* C)
 	}
 }
 
-void computeBezPt(GLfloat u, wcPt3D* bezPt, GLint nCtrlPts, wcPt3D* ctrlPts, GLint* C)
+void bino_temp(GLint n, GLint* C)
 {
-	GLint k, n = nCtrlPts - 1;
-	GLfloat bezBlendFcn;
-	bezPt->x = bezPt->y = bezPt->z = 0.0;
-	for (k = 0; k < nCtrlPts; k++)
+	GLint k, j;
+	for (k = 0; k <= n; k++)
 	{
-		bezBlendFcn = C[k] * pow(u, k) * pow(1 - u, n - k);
-		bezPt->x += ctrlPts[k].x * bezBlendFcn;
-		bezPt->y += ctrlPts[k].y * bezBlendFcn;
-		bezPt->z += ctrlPts[k].z * bezBlendFcn;
+		C[k] = 1;
+		for (j = n; j >= k + 1; j--)
+			C[k] *= j;
+		for (j = n - k; j >= 2; j--)
+			C[k] /= j;
 	}
 }
 
